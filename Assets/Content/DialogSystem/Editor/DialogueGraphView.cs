fd100d8d7473d34b2afe713129d0afbbe104bd09
Corentinrmt <corentin.remot@gmail.com>
2025-01-14 12:07:36 +0100

    [ADD]setup graph view

---
+++ Assets/Content/DialogSystem/Editor/DialogueGraphView.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+using System;
+using System.Reflection;
+
+public class DialogueGraphView : GraphView
+{
+    public DialogueGraphView()
+    {
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+
+        AddElement(GenererateEntryPointNode());
+    }
+
+
+    private Port GeneratePort(DialogueNode node, Direction portDirection, Port.Capacity capacity = Port.Capacity.Single)
+    {
+        return node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, typeof(float));
+    }
+
+    private DialogueNode GenererateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "START",
+            GIUD = Guid.NewGuid().ToString(),
+            DialogueText = "ENTRYPOINT",
+            EntryPoint = true
+        };
+
+        var generatedPort = GeneratePort(node, Direction.Output);
+        generatedPort.portName = "Next";
+        node.outputContainer.Add(generatedPort);
+
+        node.RefreshExpandedState();
+        node.RefreshPorts();
+
+        node.SetPosition(new Rect(100, 200, 100, 150));
+
+        return node;
+    }
+    public void CreateDialogueNode()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Content/DialogSystem/Editor/DialogueNode.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+
+
+public class DialogueNode : Node
+{
+    public string GIUD;
+
+    public string DialogueText;
+
+    public bool EntryPoint = false;
+}
