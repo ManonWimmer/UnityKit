6dd80f1fc6b2d9d1ce7e34c5b66e96c24688b035
Manon Wimmer <manonwimmer30@gmail.com>
2025-01-15 11:59:24 +0100

    [ADD] oups

--- Assets/Content/SaveSystem/Scripts/SaveData.cs
+++ Assets/Content/SaveSystem/Scripts/SaveData.cs
@@ -6,9 +6,6 @@ using static SaveManager;
 [System.Serializable]
 public class SaveData
 {
-    //public List<ScriptSelection> scriptSelections = new List<ScriptSelection>();
-    //public SaveData (List<ScriptSelection> scriptSelections) {  this.scriptSelections = scriptSelections; }
-
     public Dictionary<string, object> data;
     public SaveData(Dictionary<string, object> data) { this.data = data; }
 }
--- Assets/Content/SaveSystem/Scripts/SaveSystem.cs
+++ Assets/Content/SaveSystem/Scripts/SaveSystem.cs
@@ -7,22 +7,6 @@ using System.Runtime.Serialization;
 
 public static class SaveSystem
 {
-    /*
-    public static void Save(List<ScriptSelection> ScriptSelections)
-    {
-        BinaryFormatter binaryFormatter = new BinaryFormatter();
-
-        string path = Application.persistentDataPath + "/player.test";
-        FileStream stream = new FileStream(path, FileMode.Create);
-
-        SaveData data = new SaveData(ScriptSelections);
-        //Dictionary<string, object> gameData = new Dictionary<string, object>();
-
-        binaryFormatter.Serialize(stream, data);
-        stream.Close();
-    }
-    */
-
     public static void Save(Dictionary<string, object> Data)
     {
         BinaryFormatter binaryFormatter = new BinaryFormatter();
